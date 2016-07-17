3018f4814ae90fe653678c40a8e2123a47628aa5
Roy Allen Sutton <royasutton@hotmail.com>
2016-07-16 22:48:02 -0500

    Updated .gitignore and autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,11 +4,13 @@ basepath="${0%/*}"
 basename="${0##*/}"
 rootname="${basename%.*}"
 
+autoreconf_opts="--force --install -I m4"
+
 echo "${rootname} begin"
 
 # auto-configure if needed
 if [ ! -x configure ] ; then
-  autoreconf --force --install -I m4 || exit 1
+  autoreconf ${autoreconf_opts} || exit 1
 fi
 
 echo "${rootname} end"
